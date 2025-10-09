0ce954f79e2b01649d9650ab4fa40e875c00b1dd
p-probst <p.probst@campus.fct.unl.pt>
2025-10-09 18:12:22 +0100

    minor changes

--- HAR/dl/train_test.py
+++ HAR/dl/train_test.py
@@ -38,7 +38,7 @@ VAL_LOSS_KEY = "val_loss"
 # public functions
 # ------------------------------------------------------------------------------------------------------------------- #
 def run_model_training(
-                       model: nn.Module, model_save_path: str,
+                       model: nn.Module, model_save_path: str, model_name: str,
                        train_dataloader: torch.utils.data.DataLoader, test_dataloader: torch.utils.data.DataLoader,
                        criterion: nn.Module, optimizer: torch.optim.Optimizer,
                        cuda_device: torch.device, num_epochs: int, patience: int = 5) -> dict[str, list[float]]:
@@ -47,6 +47,7 @@ def run_model_training(
     The loop is terminated using early stopping in case the performance does not improve according to the set patience.
     :param model: PyTorch model to be trained
     :param model_save_path: file path to where the model is stored
+    :param model_name: the name of the model as a str
     :param train_dataloader: PyTorch DataLoader for training data
     :param test_dataloader: PyTorch DataLoader for validation data
     :param criterion: loss function to be used
@@ -99,11 +100,8 @@ def run_model_training(
             # reset early-stopping counter
             epochs_no_improve = 0
 
-            # obtain model name
-            model_name = f"{model.__class__.__name__}_{model.model_type}_hs{model.hidden_size}_nl{model.num_layers}_do{int(model.dropout * 100)}.pt"
-
             # store best model params
-            torch.save(model.state_dict(), os.path.join(model_save_path, model_name))
+            torch.save(model.state_dict(), os.path.join(model_save_path,  f"{model_name}.pt"))
             print(f"INFO: New best model obtained at epoch {epoch} (val. acc: {best_val_acc:.4f})")
 
         else: # no improvement
